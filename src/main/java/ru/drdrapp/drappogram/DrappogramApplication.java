8da35c211801b33a04765ffd59a691389556a305
drDrapp <born--in--ussr@mail.ru>
2023-04-06 08:42:48 +0300

    Initial commit

---
+++ src/main/java/ru/drdrapp/drappogram/DrappogramApplication.java
@@ -0,0 +1,13 @@
+package ru.drdrapp.drappogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrappogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrappogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/drdrapp/drappogram/DrappogramApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.drdrapp.drappogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrappogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
