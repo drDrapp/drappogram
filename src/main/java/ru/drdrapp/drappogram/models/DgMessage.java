4b65d758505d9cd6553b959ceb988075f60ae8aa
drDrapp <born--in--ussr@mail.ru>
2023-05-26 16:39:38 +0300

    + Add column name + Add init sql + Add dependency

--- src/main/java/ru/drdrapp/drappogram/models/DgMessage.java
+++ src/main/java/ru/drdrapp/drappogram/models/DgMessage.java
@@ -1,6 +1,8 @@
 package ru.drdrapp.drappogram.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -12,14 +14,18 @@ import lombok.*;
 @ToString
 @Table(name = "dg_message")
 public class DgMessage {
+
     public static final int START_SEQ = 10;
+
     @Id
+    @Column(name = "id", nullable = false)
+    @NotNull
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dg_message_gen")
     @SequenceGenerator(name = "dg_message_gen", sequenceName = "dg_message_seq", initialValue = DgMessage.START_SEQ, allocationSize = 1)
-    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "text")
+    @Column(name = "text", nullable = false)
+    @NotBlank(message = "Text may not be blank")
     private String text;
 
     @Column(name = "tag")
@@ -32,7 +38,7 @@ public class DgMessage {
     @JoinColumn(name = "user_id")
     private DgUser author;
 
-    public DgMessage(String text, String tag, DgUser author) {
+    public DgMessage(@NonNull String text, String tag, DgUser author) {
         this.text = text;
         this.tag = tag;
         this.author = author;
--- src/main/java/ru/drdrapp/drappogram/models/DgUser.java
+++ src/main/java/ru/drdrapp/drappogram/models/DgUser.java
@@ -1,6 +1,8 @@
 package ru.drdrapp.drappogram.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.Set;
@@ -12,25 +14,43 @@ import java.util.Set;
 @Entity
 @Table(name = "dg_user")
 public class DgUser {
+
     public static final int START_SEQ = 10;
+
     @Id
+    @Column(name = "id", nullable = false)
+    @NotNull
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "dg_user_gen")
     @SequenceGenerator(name = "dg_user_gen", sequenceName = "dg_user_seq", initialValue = DgUser.START_SEQ, allocationSize = 1)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "login", nullable = false)
+    @NotBlank(message = "Login may not be blank")
     private String login;
+
+    @Column(name = "hash_password", nullable = false)
+    @NotNull
     private String hashPassword;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "dg_user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    @Column(name = "state")
     @Enumerated(value = EnumType.STRING)
     private State state;
 
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "activation_code")
     private String activationCode;
 
 }
